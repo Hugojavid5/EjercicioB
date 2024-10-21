fae81883661a6c6099f9229d774aa482a863f623
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-21 20:24:20 +0200

    Clase Hello application sin javadocs

--- src/main/java/org/hugo/ejerciciob/HelloApplication.java
+++ src/main/java/org/hugo/ejerciciob/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EjercicioC.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EjercicioB.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Image icon = new Image(getClass().getResourceAsStream("/Imagenes/icono.png"));
         stage.getIcons().add(icon);
