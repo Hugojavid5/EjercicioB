31d6a9bae6ecc8d8741500748a92aa032f787af3
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-24 16:43:16 +0200

    Clase HelloApplication acabada con javadocs

--- src/main/java/org/hugo/ejerciciob/HelloApplication.java
+++ src/main/java/org/hugo/ejerciciob/HelloApplication.java
@@ -8,22 +8,48 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase principal de la aplicación JavaFX.
+ * Esta clase extiende la clase {@link Application} y maneja la inicialización
+ * y configuración de la interfaz gráfica de la aplicación.
+ */
 public class HelloApplication extends Application {
+
+    /**
+     * Inicia la aplicación JavaFX.
+     * @param stage La ventana principal de la aplicación.
+     * @throws IOException Si no se puede cargar el archivo FXML.
+     */
     @Override
     public void start(Stage stage) throws IOException {
+        // Carga el archivo FXML para construir la interfaz gráfica
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EjercicioB.fxml"));
+
+        // Crea una nueva escena con el contenido del archivo FXML
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+
+        // Carga el ícono de la aplicación
         Image icon = new Image(getClass().getResourceAsStream("/Imagenes/icono.png"));
         stage.getIcons().add(icon);
+
+        // Establece las dimensiones máximas y mínimas de la ventana
         stage.setMaxWidth(840);
         stage.setMinWidth(565);
         stage.setMinHeight(325);
-        stage.setTitle("PERSONAS");
+
+        // Configura el título de la ventana
+        stage.setTitle("Ejercicio B!");
+
+        // Establece la escena en el escenario y muestra la ventana
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Método principal que lanza la aplicación JavaFX.
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String[] args) {
-        launch();
+        launch(); // Lanza la aplicación
     }
 }
