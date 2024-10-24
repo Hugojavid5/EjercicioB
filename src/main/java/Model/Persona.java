aaee3f50e1564b117d1b9d75793108d59b176813
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-24 16:41:31 +0200

    Clase Persona acabada con javadocs

--- src/main/java/Model/Persona.java
+++ src/main/java/Model/Persona.java
@@ -3,29 +3,28 @@ package Model;
 import java.util.Objects;
 
 /**
- * La clase Persona representa una persona con nombre, apellidos y edad.
- * Proporciona métodos para acceder y modificar estos atributos.
+ * Clase Persona que representa una persona con un nombre, apellido y edad.
  */
 public class Persona {
     private String nombre;
-    private String apellidos;
+    private String apellido;
     private int edad;
 
     /**
-     * Constructor de la clase Persona.
-     * @param nom      El nombre de la persona.
-     * @param apellidos Los apellidos de la persona.
-     * @param edad     La edad de la persona.
+     * Constructor que inicializa una persona con nombre, apellido y edad.
+     * @param nombre   Nombre de la persona.
+     * @param apellido Apellido de la persona.
+     * @param edad     Edad de la persona.
      */
-    public Persona(String nom, String apellidos, int edad) {
-        this.nombre = nom;
-        this.apellidos = apellidos;
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
         this.edad = edad;
     }
 
     /**
      * Obtiene el nombre de la persona.
-     * @return El nombre de la persona.
+     * @return Nombre de la persona.
      */
     public String getNombre() {
         return nombre;
@@ -33,31 +32,32 @@ public class Persona {
 
     /**
      * Establece el nombre de la persona.
-     * @param nombre El nuevo nombre de la persona.
+     * @param nombre Nombre de la persona.
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * Obtiene los apellidos de la persona.
-     * @return Los apellidos de la persona.
+     * Obtiene el apellido de la persona.
+
+     * @return Apellido de la persona.
      */
-    public String getApellidos() {
-        return apellidos;
+    public String getApellido() {
+        return apellido;
     }
 
     /**
-     * Establece los apellidos de la persona.
-     * @param apellidos Los nuevos apellidos de la persona.
+     * Establece el apellido de la persona.
+     * @param apellido Apellido de la persona.
      */
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
     /**
      * Obtiene la edad de la persona.
-     * @return La edad de la persona.
+     * @return Edad de la persona.
      */
     public int getEdad() {
         return edad;
@@ -65,17 +65,17 @@ public class Persona {
 
     /**
      * Establece la edad de la persona.
-     * @param edad La nueva edad de la persona.
+     * @param edad Edad de la persona.
      */
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
     /**
-     * Compara este objeto Persona con otro para verificar si son iguales.
-     * Dos objetos Persona se consideran iguales si tienen el mismo contenido de los atributos.
-     * @param o El objeto a comparar.
-     * @return true si los objetos son iguales, false en caso contrario.
+     * Sobrescribe el método equals para comparar si dos personas son iguales
+     * basándose en su nombre, apellido y edad.
+     * @param o Objeto a comparar.
+     * @return true si los objetos son iguales, false de lo contrario.
      */
     @Override
     public boolean equals(Object o) {
@@ -84,15 +84,17 @@ public class Persona {
         Persona persona = (Persona) o;
         return edad == persona.edad &&
                 Objects.equals(nombre, persona.nombre) &&
-                Objects.equals(apellidos, persona.apellidos);
+                Objects.equals(apellido, persona.apellido);
     }
 
     /**
-     * Devuelve un código hash basado en los atributos nombre, apellidos y edad.
-     * @return El código hash para este objeto.
+     *
+     * Sobrescribe el metodo HashCode para convertir un hash
+     * en el nombre, apellido y edad de la persona.
+     * @return Código hash de la persona.
      */
     @Override
     public int hashCode() {
-        return Objects.hash(nombre, apellidos, edad);
+        return Objects.hash(nombre, apellido, edad);
     }
 }
