07ba8d1ce1fd0b97139b7b5c7f48a7220cafdbf0
Hugo <hugojavidferrero@gmail.com>
2024-10-28 09:41:22 +0100

    Ejercico corregido

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,7 @@ module org.hugo.ejerciciob {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-
-
-    opens org.hugo.ejerciciob to javafx.fxml;
+    opens Model to javafx.fxml , javafx.base;
+    opens org.hugo.ejerciciob to javafx.base, javafx.fxml;
     exports org.hugo.ejerciciob;
 }
